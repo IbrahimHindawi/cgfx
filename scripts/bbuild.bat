3d0bbe73145629f059be632233aa1bb32fce60e7
IbrahimHindawi <ihindawi@gmail.com>
2023-07-22 15:26:55 +0400

    integrated vcpkg with cmake

--- scripts/bbuild.bat
+++ scripts/bbuild.bat
@@ -3,7 +3,7 @@
 mkdir build
 pushd build
 
-cmake .. -G "Visual Studio 17 2022"
+cmake .. -G "Visual Studio 17 2022" "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake"
 powershell -Command ..\scripts\clang-build.ps1 -export-jsondb
 
 popd build
