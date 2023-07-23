dddd1c9c10b10620d2e3d5a39e5478193075cd0a
IbrahimHindawi <ihindawi@gmail.com>
2023-07-23 12:06:48 +0400

    loaded opengl and added exit event

--- source/main.c
+++ source/main.c
@@ -1,6 +1,7 @@
+#include "SDL2/SDL_events.h"
+#include "SDL2/SDL_video.h"
 #ifdef _MSC_VER
     #define SDL_MAIN_HANDLED
-    // #include <windows.h>
 #endif
 
 #include <math.h>
@@ -12,32 +13,44 @@
 #include <glad/glad.h>
 #include <SDL2/SDL.h>
 
-// #include <cglm/affine.h>
-// #include <cglm/cam.h>
-// #include <cglm/mat4.h>
-// #include <cglm/vec3.h>
 #include <linmath.h>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#define uint8_t u8
+#define int8_t i8
+#define int16_t i16
+#define uint16_t u16
+#define int32_t i32
+#define uint32_t u32
+#define int64_t i64
+#define uint64_t u64
+#define size_t usize
+
 
 int main(int argc, char *argv[]) {
+    /*
+     * Setup OpenGL
+     */
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
     /* Initialises data */
     SDL_Window *window = NULL;
 
     /*
-    * Initialises the SDL video subsystem (as well as the events subsystem).
-    * Returns 0 on success or a negative error code on failure using SDL_GetError().
-    */
+     * Initialises the SDL video subsystem (as well as the events subsystem).
+     * Returns 0 on success or a negative error code on failure using SDL_GetError().
+     */
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
         return 1;
     }
 
     /* Creates a SDL window */
-    window = SDL_CreateWindow("SDL Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 640, 0);
+    window = SDL_CreateWindow("cgfx", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 640, SDL_WINDOW_OPENGL);
 
     /* Checks if window has been created; if not, exits program */
     if (window == NULL) {
@@ -45,8 +58,32 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    /* Pauses all SDL subsystems for a variable amount of milliseconds */
-    SDL_Delay(3000);
+    SDL_GL_CreateContext(window);
+    if (!gladLoadGLLoader((GLADloadproc)SDL_GL_GetProcAddress)) {
+        fprintf(stderr, "Failed to load GL: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    puts("OpenGL loaded");
+    printf("Vendor: %s\n", glGetString(GL_VENDOR));
+    printf("Renderer: %s\n", glGetString(GL_RENDERER));
+    printf("Version: %s\n", glGetString(GL_VERSION));
+
+    bool should_quit = false;
+    while (!should_quit) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT: {
+                    should_quit = true;
+                    break;
+                }
+                default: {
+                    break;
+                }
+            }
+        }
+    }
 
     /* Frees memory */
     SDL_DestroyWindow(window);
