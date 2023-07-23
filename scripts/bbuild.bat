292dbec54067f34d1ef093eceee3384215270fa8
IbrahimHindawi <ihindawi@gmail.com>
2023-07-23 16:00:36 +0400

    added python workaround

--- scripts/bbuild.bat
+++ scripts/bbuild.bat
@@ -5,5 +5,6 @@ pushd build
 
 cmake .. -G "Visual Studio 17 2022" "-DCMAKE_TOOLCHAIN_FILE=C:\devel\vcpkg\scripts\buildsystems\vcpkg.cmake"
 powershell -Command ..\scripts\clang-build.ps1 -export-jsondb
+python ..\scripts\finalize.py
 
 popd build
